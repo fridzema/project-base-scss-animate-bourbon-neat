ac53719c9300ec2acf176e9ddd9dcaedfa752eb6
Robert Fridzema <fridzema@gmail.com>
2015-02-07 12:50:46 +0100

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,36 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-(function () {
-  'use strict';
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-  var querySelector = document.querySelector.bind(document);
+    var ractive = new Ractive({
+      // The `el` option can be a node, an ID, or a CSS selector.
+      el: 'container',
 
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
+      // We could pass in a string, but for the sake of convenience
+      // we're passing the ID of the <script> tag above.
+      template: '#template-page',
 
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
+      // Here, we're passing in some initial data
+      data: {
+        content: 'BLalblal'
+        }
+    });
 
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
+    $("nav ul li a").on( "click", function(e) {
+      e.preventDefault();
+      var linkName = $(this).attr("href");
+      
+      console.log(linkName);
 
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
-})();
+      var contactContent = "Contact";
+      var homeContent = "Home";
+
+      if(linkName == "contact") {
+        ractive.set( 'content', contactContent);
+      }
+
+      if(linkName == "home") {
+        ractive.set( 'content', homeContent);
+      }
+
+    });
+});
